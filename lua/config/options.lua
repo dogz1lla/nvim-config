084e52b90eb376399825f6603361864eb1456762
gz <zarubin.grigory@gmail.com>
2025-02-17 22:45:19 +0100

    minor linting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,26 +1,26 @@
 local options = {
     -- copied from bread's config
-	ruler = false, --disable extra numbering
-	showmode = false, --not needed due to lualine
-	showcmd = false,
-	wrap = true, --toggle bound to leader W
-	mouse = "a", --enable mouse
-	clipboard = "unnamedplus", --system clipboard integration
-	history = 100, --command line history
-	swapfile = false, --swap just gets in the way, usually
-	backup = false,
-	cursorline = true, --highlight line
-	ttyfast = true, --faster scrolling
-	smoothscroll = true,
-	
-	foldmethod = "expr",
-	foldlevel = 99, --disable folding, lower #s enable
-	foldexpr = "nvim_treesitter#foldexpr()",
-	
-	termguicolors = false,
+    ruler = false, --disable extra numbering
+    showmode = false, --not needed due to lualine
+    showcmd = false,
+    wrap = true, --toggle bound to leader W
+    mouse = "a", --enable mouse
+    clipboard = "unnamedplus", --system clipboard integration
+    history = 100, --command line history
+    swapfile = false, --swap just gets in the way, usually
+    backup = false,
+    cursorline = true, --highlight line
+    ttyfast = true, --faster scrolling
+    smoothscroll = true,
 
-	ignorecase = true, --ignore case while searching
-	smartcase = true, --but do not ignore if caps are used
+    foldmethod = "expr",
+    foldlevel = 99, --disable folding, lower #s enable
+    foldexpr = "nvim_treesitter#foldexpr()",
+
+    termguicolors = false,
+
+    ignorecase = true, --ignore case while searching
+    smartcase = true, --but do not ignore if caps are used
 
     -- my custom stuff
     tabstop = 4,
@@ -37,11 +37,11 @@ local options = {
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 vim.diagnostic.config({
-	signs = false,
+    signs = false,
 })
 
 -- vertical line at 'colorcolumn' symbols
